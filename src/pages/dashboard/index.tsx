d0f3bfb39c700e2734b529f1e4f89812b158d5b1
nvhai982006 <nguyenvanhai982006@gmail.com>
2025-11-03 22:55:09 +0700

    up

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,10 @@
 const DashBoard = () => {
   return (
     <div>
-      <p>bao la cau be dao made in china</p>
-      <p>bao la cau be dao made in china</p>
-      <p>bao la cau be dao made in china</p>
-      <p>bao la cau be dao made in china</p>
+      <p>Bảo là cậu bé Down made in china</p>
+      <p>Bảo là cậu bé Down made in china</p>
+      <p>Bảo là cậu bé Down made in china</p>
+      <p>Bảo là cậu bé Down made in china</p>
     </div>
   )
 }
